43733e41e1ba8693aecfcdcbe55f14f6af584d46
Khoa Pham <onmyway133@gmail.com>
2017-12-20 18:36:31 +0100

    Add author info

--- AddressGeneratorTests/AddressGeneratorTests.swift
+++ AddressGeneratorTests/AddressGeneratorTests.swift
@@ -1,3 +1,10 @@
+//
+//  AddressGenerator
+//
+//  Created by Khoa Pham on 14.12.2017.
+//  Copyright © 2017 Khoa Pham. All rights reserved.
+//
+
 import XCTest
 @testable import AddressGenerator
 
