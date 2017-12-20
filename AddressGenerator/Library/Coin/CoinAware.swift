03b6ce5a187411f2c22631fa3c310e770c3a16bb
Khoa Pham <onmyway133@gmail.com>
2017-12-20 18:38:00 +0100

    Add CoinAware protocol

---
+++ AddressGenerator/Library/Coin/CoinAware.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+struct Account {
+  let rawPrivateKey: String
+  let rawPublicKey: String
+  let address: String
+  let walletImportFormat: String
+}
+
+protocol CoinAware {
+  var name: String { get }
+  func generate() -> Account
+}
