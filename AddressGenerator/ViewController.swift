c48a12e09371d3ca193e59a78244817f8518450b
Khoa Pham <onmyway133@gmail.com>
2017-12-21 12:23:45 +0100

    Add Anchors

--- AddressGenerator/ViewController.swift
+++ AddressGenerator/ViewController.swift
@@ -7,11 +7,36 @@
 //
 
 import Cocoa
+import Anchors
 
 class ViewController: NSViewController {
 
+  let coinsController = CoinsController()
+  let accountController = AccountController()
+
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    setup()
+  }
+
+  func setup() {
+    addChildViewController(coinsController)
+    view.addSubview(coinsController.view)
+
+    addChildViewController(accountController)
+    view.addSubview(accountController.view)
+
+    activate(
+      coinsController.view.anchor.left.top.constant(10),
+      coinsController.view.anchor.bottom.constant(-10),
+
+      coinsController.view.anchor.right.equal
+        .to(accountController.view.anchor.left).constant(10),
+
+      accountController.view.anchor.top.constant(10),
+      accountController.view.anchor.right.bottom.constant(-10)
+    )
   }
 }
 
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :osx, '10.11'
+
+target 'AddressGenerator' do
+  use_frameworks!
+  pod 'Anchors'
+end
