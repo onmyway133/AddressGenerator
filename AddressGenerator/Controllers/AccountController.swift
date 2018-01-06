75a7c1b9b254861f2766032aa7db79d750b05c8b
Khoa Pham <onmyway133@gmail.com>
2018-01-06 18:44:39 +0100

    Add many labels

--- AddressGenerator/Controllers/AccountController.swift
+++ AddressGenerator/Controllers/AccountController.swift
@@ -10,7 +10,50 @@ import AppKit
 import Anchors
 
 final class AccountController: BaseController {
+  private let coinNameLabel = Label()
+  private let button = NSButton()
+  private let addressImageView = NSImageView()
+
+  private let addressLabel = Label()
+  private let addressValueLabel = Label()
+
+  private let publicKeyLabel = Label()
+  private let publicKeyValueLabel = Label()
+
+  private let privateKeyLabel = Label()
+  private let privateKeyValueLabel = Label()
+
+  private let wifLabel = Label()
+  private let wifValueLabel = Label()
+
+  var coin: CoinAware? {
+    didSet {
+      coinNameLabel.stringValue = coin?.name ?? ""
+    }
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    setup()
+  }
+
+  private func setup() {
+    view.e_addSubviews([
+      coinNameLabel, button, addressImageView,
+      addressLabel, addressValueLabel,
+      publicKeyLabel, publicKeyValueLabel,
+      privateKeyLabel, privateKeyValueLabel,
+      wifLabel, wifValueLabel
+    ])
+
+    setupConstraints()
+  }
+
+  private func setupConstraints() {
+    activate(
+      coinNameLabel.anchor.top,
+      coinNameLabel.anchor.centerX
+    )
   }
 }
