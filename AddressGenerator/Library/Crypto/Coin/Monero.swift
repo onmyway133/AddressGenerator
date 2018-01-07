8af19707a93eb7283ee077b7c12b5a80269f1336
Khoa Pham <onmyway133@gmail.com>
2018-01-07 23:33:57 +0100

    Add Monero

---
+++ AddressGenerator/Library/Crypto/Coin/Monero.swift
@@ -0,0 +1,18 @@
+//
+//  Monero.swift
+//  AddressGenerator
+//
+//  Created by Khoa Pham on 07.01.2018.
+//  Copyright © 2018 Khoa Pham. All rights reserved.
+//
+
+import Foundation
+
+// https://getmonero.org/
+struct Monero: CoinAware {
+  let name = "Monero"
+
+  func generate() throws -> Account {
+    fatalError()
+  }
+}
---
+++ AddressGenerator/Library/Crypto/Engine/MoneroGenerator.swift
@@ -0,0 +1,16 @@
+//
+//  MoneroGenerator.swift
+//  AddressGenerator
+//
+//  Created by Khoa Pham on 07.01.2018.
+//  Copyright © 2018 Khoa Pham. All rights reserved.
+//
+
+import Foundation
+
+// https://getmonero.org/resources/moneropedia/address.html
+struct MoneroGenerator {
+  func generate() throws -> Data {
+    fatalError()
+  }
+}
